b09386af8dab89833487db8953d60f3ac88b933c
zihan zhao <zihanhannah@gmail.com>
2020-12-25 19:03:51 -0600

    adding commit to it

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,4 +1,20 @@
 public class ValidPalindrome {
+//  https://leetcode.com/problems/valid-palindrome/
+//    number: 125
+//    using two pointers two-side
+//    Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+//
+//Note: For the purpose of this problem, we define empty string as valid palindrome.
+//
+//Example 1:
+//
+//Input: "A man, a plan, a canal: Panama"
+//Output: true
+//Example 2:
+//
+//Input: "race a car"
+//Output: false
+
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
         int left = 0;
