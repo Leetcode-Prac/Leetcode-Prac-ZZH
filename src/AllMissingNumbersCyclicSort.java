f649aee65468698c6c42b7632b2d5ec3dabb9a54
zihan zhao <zihanhannah@gmail.com>
2021-01-08 19:31:58 -0600

    get the all missing numbers from the array by using cyclic sort

---
+++ src/AllMissingNumbersCyclicSort.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//We are given an unsorted array containing numbers taken from the range 1 to ‘n’. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+//Example 1:
+//Input: [2, 3, 1, 8, 2, 3, 5, 1]
+//Output: 4, 6, 7
+//Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+//Example 2:
+//Input: [2, 4, 1, 2]
+//Output: 3
+//Example 3:
+//Input: [2, 3, 2, 1]
+//Output: 4
+public class AllMissingNumbersCyclicSort {
+    public static List<Integer> findNumbers(int[] nums){
+        List<Integer> missingNumbers = new ArrayList<>();
+        int i = 0;
+        while(i< nums.length){
+//            tricky!!
+            if(nums[i] != nums[nums[i] - 1 ]){
+                int j = nums[i] -1;
+                swap(nums,i,j);
+            } else {
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        for(int k = 0; k < nums.length; k++){
+            if(nums[k] != k + 1){
+                missingNumbers.add(k + 1);
+            }
+        }
+        return missingNumbers;
+    }
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> missing = AllMissingNumbersCyclicSort.findNumbers(new int[] { 2, 3, 1, 8, 2, 3, 5, 1 });
+        System.out.println("Missing numbers: " + missing);
+
+        missing = AllMissingNumbersCyclicSort.findNumbers(new int[] { 2, 4, 1, 2 });
+        System.out.println("Missing numbers: " + missing);
+
+        missing = AllMissingNumbersCyclicSort.findNumbers(new int[] { 2, 3, 2, 1 });
+        System.out.println("Missing numbers: " + missing);
+    }
+
+}
