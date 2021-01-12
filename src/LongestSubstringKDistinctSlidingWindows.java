4a64721b5c8e7a0d6fa780e3d5850b87a6131be3
zihan zhao <zihanhannah@gmail.com>
2021-01-11 22:06:50 -0600

    sliding window to get the max length with K distict value

---
+++ src/LongestSubstringKDistinctSlidingWindows.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class LongestSubstringKDistinctSlidingWindows {
+    public static int findLength(String str, int k) {
+        if (str == null || str.length() == 0 || str.length() < k)
+            throw new IllegalArgumentException();
+        int windowStart = 0, maxLength = 0;
+//        HashMap only store Unique key!!! not duplicated key
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        // in the following loop we'll try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            charFrequencyMap.put(rightChar, charFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            // shrink the sliding window, until we are left with 'k' distinct characters in the frequency map
+            while (charFrequencyMap.size() > k) {
+                char leftChar = str.charAt(windowStart);
+                charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) - 1);
+                if (charFrequencyMap.get(leftChar) == 0) {
+                    charFrequencyMap.remove(leftChar);
+                    System.out.println(charFrequencyMap);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1); // remember the maximum length so far
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinctSlidingWindows.findLength("araaci", 2));
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinctSlidingWindows.findLength("araaci", 1));
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinctSlidingWindows.findLength("cbbebi", 3));
+    }
+}
